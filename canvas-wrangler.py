4460a14a2222f11d41bf34cf2a132969af2b8c03
John Hui <jwongsquared@gmail.com>
2018-10-11 00:40:57 -0400

    added verbose options to canvas wrangler, and no grade/no comment options

--- canvas-wrangler.py
+++ canvas-wrangler.py
@@ -7,7 +7,6 @@ import requests
 import canvaslib as cvl
 import boatswain_env as benv
 import logging
-logging.basicConfig(level=logging.INFO)
 
 CMD_NAME = 'canvaswrangler'
 
@@ -18,14 +17,16 @@ def make_parser():
     parser.add_argument('-G', '--grade-col',
                         default='grade',
                         type=str,
-                        help='set name for grade header column',
+                        help='set name for grade header column, '
+                            'pass empty string to not submit grades',
                         metavar='<header>',
     )
 
     parser.add_argument('-C', '--comment-col',
                         default='comment',
                         type=str,
-                        help='set name for comment header column',
+                        help='set name for comment header column, '
+                            'pass empty string to not submit grades',
                         metavar='<header>',
     )
 
@@ -53,6 +54,7 @@ def make_parser():
                         metavar='<grades.csv>',
     )
 
+    '''
     # warning log options
     parser.add_argument('-L', '--log',
                         default='',
@@ -65,6 +67,7 @@ def make_parser():
                         action='store_true',
                         help='do not produce log file for warnings',
     )
+    '''
 
     # testing options
     parser.add_argument('-n', '--noop',
@@ -73,6 +76,13 @@ def make_parser():
                         help='do not submit grades; for testing purposes',
     )
 
+    parser.add_argument('-v', '--verbose',
+                        default=False,
+                        action='store_true',
+                        help='turn on verbose output; for debugging',
+    )
+
+
     return parser
 
 
@@ -80,44 +90,49 @@ def retrieve_index(header_row, target):
     for i, cell in enumerate(header_row):
         if cell == target:
             return i
-    return None
+    raise LookupError('uni index not found')
 
 
-# TODO: make (comment & grade) index (grade | index)
-# not necessary to have both
 def retrieve_indices(header, student_col, grade_col, comment_col):
-    student_idx = retrieve_index(header, student_col)
-    if student_idx is None:
-        raise IndexException('uni index not found')
+    if student_col == '':
+        raise LookupError('must specify student column header value')
     else:
+        student_idx = retrieve_index(header, student_col)
         logging.debug('student index: %s', student_idx)
 
-    grade_idx = retrieve_index(header, grade_col)
-    if grade_idx is None:
-        raise IndexException('grade index not found')
+    if grade_col == '' and comment_col == '':
+        raise LookupError('neither grade nor column specified')
+
+    if grade_col == '':
+        grade_idx = None
+        logging.debug('empty grade column specified, not submitting grades')
     else:
+        grade_idx = retrieve_index(header, grade_col)
         logging.debug('grade index: %s', grade_idx)
 
-    comment_idx = retrieve_index(header, comment_col)
-    if comment_idx is None:
-        raise IndexException('comment index not found')
+    if comment_col == '':
+        comment_idx = None
+        logging.debug('empty comment column specified, not submitting comments')
     else:
+        comment_idx = retrieve_index(header, comment_col)
         logging.debug('comment index: %s', comment_idx)
+
+    logging.debug('')
 
     return student_idx, grade_idx, comment_idx
 
-def build_gradesheet(grades, student_col, grade_col=None, comment_col=None):
+def build_gradesheet(grades, student_idx, grade_idx=None, comment_idx=None):
     gradesheet = []
     for i, row in enumerate(grades):
-        user_id = row[student_col]
+        user_id = row[student_idx]
 
-        if grade_col is not None:
-            grade = row[grade_col]
+        if grade_idx is not None:
+            grade = row[grade_idx]
         else:
             grade = None
 
-        if comment_col is not None:
-            comment = row[comment_col]
+        if comment_idx is not None:
+            comment = row[comment_idx]
         else:
             comment = None
         gradesheet.append(cvl.GradesheetEntry(user_id, grade, comment))
@@ -167,18 +182,29 @@ def submit_grades(url, data, headers):
     return response.status_code
 
 
-# TODO: set noop to false
+def log_dump_wrangler(url, data, headers, opt):
+    if opt.verbose:
+        logging.debug('API URL: {}'.format(url))
+        logging.debug('Headers: {}'.format(headers))
+        logging.debug('Data: {')
+        for k in data:
+            logging.debug('\t{} : {}'.format(k, data[k]))
+        logging.debug('}')
+        logging.debug('')
+
 def wrangle_canvas(token, grades, opt):
     header = next(grades)
 
-    student_idx, grade_idx, comment_idx = retrieve_indices(header, opt.student_col,
-            opt.grade_col, opt.comment_col)
+    student_idx, grade_idx, comment_idx = retrieve_indices(
+            header, opt.student_col, opt.grade_col, opt.comment_col)
 
     gradesheet = build_gradesheet(grades, student_idx, grade_idx, comment_idx)
     
     headers = cvl.auth_header(token)
     uri, data = cvl.update_grades(opt.course_id, opt.assignment_id, gradesheet)
     url = cvl.format_url(uri)
+
+    log_dump_wrangler(url, data, headers, opt)
 
     if opt.noop:
         logging.info('--noop option specified; not submitting to Canvas.')
@@ -191,6 +217,11 @@ def main(args=None, config_path=None, verbose=True):
         args = sys.argv[1:]
 
     opt, config = benv.envParse(CMD_NAME, make_parser(), args, config_path)
+
+    if opt.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
 
     grades = csv.reader(opt.grades)
 
