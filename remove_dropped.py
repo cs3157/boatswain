a5e65f76d576b355bf7f8fea371e6ef6b7a10d8c
pymlu16 <phoebe.yumin.lu@gmail.com>
2023-02-09 19:49:16 -0500

    removes successfully

--- remove_dropped.py
+++ remove_dropped.py
@@ -7,39 +7,77 @@ import csv
 import os
 from github import Github
 
-parser = argparse.ArgumentParser(
-    description='Remove dropped students from Github Team Org')
+CMD_NAME = 'drop_students'
+DESC = 'Remove dropped students from GitHub team repo'
 
-parser.add_argument('roster',
+def drop_students_deco(parser):
+    parser.add_argument('owner',
+                    type=str,
+                    help='repo owner (individual or organization)',
+                    metavar='<repo-owner>',
+    )
+    parser.add_argument('roster',
                     type=str,
                     help='roster CSV file',
                     nargs='?',
                     default='',
                     metavar='<roster.csv>')
 
-parser.add_argument('github_handles',
+    parser.add_argument('github_handles',
                     type=str,
                     help='matches uni to github handle CSV file',
                     nargs='?',
                     default='',
                     metavar='<uni_to_github.csv>')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-if args.roster == '':
-    parser.print_usage()
-    print(f"{parser.prog}: error: too few arguments")
-    exit()
+    if args.roster == '' or args.github_handles == '':
+        parser.print_usage()
+        print(f"{parser.prog}: error: too few arguments")
+        exit()
 
-with open(args.roster) as r:
-    roster = csv.DictReader(r)
-    roster_l = [x['SIS Login ID'] for x in roster]
+def find_and_drop_students(opt):
+    with open(opt.roster) as r:
+        roster = csv.DictReader(r)
+        roster_l = [x['SIS Login ID'] for x in roster]
 
-    with open(args.github_handles) as g:
-        handles = csv.DictReader(g)
+        with open(opt.github_handles) as g:
+            handles = csv.DictReader(g)
 
-        for row in handles:
-            if row['UNI'] not in roster_l:
-                print(row['UNI'])
+            for row in handles:
+                if row['UNI'] not in roster_l:
+                    remove_collaborator(row['GitHub Handle'], row['Group Name'],opt)
+                    
 
+def remove_collaborator(handle, group_name, opt):
+    g = Github(opt.githubToken())
+    org = g.get_organization(opt.owner)
+    print(org)
+    #repo_full_name = '{}/{}'.format(opt.owner, group_name)
+    #repo = g.get_repo(repo_full_name)
+    #user = g.get_user(handle)
 
+    if not opt.promptYes(('Are you sure you would like to remove {} '
+                            'as a member of {}?')
+                            .format(handle, org),
+                        True):
+        opt.warn('Aborting')
+        return
+    user = g.get_user(handle)
+    do_remove_collaborator(org, user, opt)
+
+def do_remove_collaborator(org, user, opt):
+    org.remove_from_members(user)
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    opt = benv.ParseOption(args, section=None, config_path=None,
+            desc=DESC, parse_deco=drop_students_deco, req_github=True)
+
+    find_and_drop_students(opt)
+
+if __name__ == '__main__':
+    main()
